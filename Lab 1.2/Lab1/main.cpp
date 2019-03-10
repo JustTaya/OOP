5d17ff7a3e5de92ba0732661047c833f50705294
Tai <justataya@gmail.com>
2019-03-10 14:26:37 +0300

    Added tests.

--- Lab 1.2/Lab1/main.cpp
+++ Lab 1.2/Lab1/main.cpp
@@ -1,6 +1,48 @@
-#include "Samples.h"
+#define CATCH_CONFIG_MAIN
+//#include "Samples.h"
+#include "IPAddress.h"
+#include "Tree.h"
+#include "catch.hpp"
 
+TEST_CASE("Test IDv6", "[IDv6]") {
+    std::string *ip6 = new std::string[8];
+    ip6[0] = "2001";
+    ip6[1] = "0db8";
+    ip6[2] = "11a3";
+    ip6[3] = "09d7";
+    ip6[4] = "1f34";
+    ip6[5] = "8a2e";
+    ip6[6] = "07a0";
+    ip6[7] = "765d";
+    IPv6 ip(ip6);
+    REQUIRE(ip.getBinary() == std::bitset<128>(
+            "00100000000000010000110110111000000100011010001100001001110101110001111100110100100010100010111000000111101000000111011001011101"));
+}
 
+TEST_CASE("Test IDv4", "[IDv4]") {
+    unsigned ip4[4];
+    ip4[0] = 209;
+    ip4[1] = 173;
+    ip4[2] = 53;
+    ip4[3] = 167;
+    IPv4 ip(ip4);
+    //REQUIRE(ip.getBinary()==std::bitset<32>("11000000000000000000001011101011"));
+    std::string *ip6 = new std::string[8];
+    ip6[0] = "0";
+    ip6[1] = "0";
+    ip6[2] = "0";
+    ip6[3] = "0";
+    ip6[4] = "0";
+    ip6[5] = "0";
+    ip6[6] = "d1ab";
+    ip6[7] = "35a7";
+    IPv6* tmp=new IPv6(ip6);
+    std::cout<<tmp->getBinary()<<"   ";
+    std::cout<<ip.converttoIPv6()->getBinary();
+    REQUIRE(ip.converttoIPv6()->getBinary() == tmp->getBinary());
+}
+
+/*
 int main() {
     srand(time(NULL));
     int_sample();
@@ -18,4 +60,4 @@ int main() {
     subnet_sample();
     return 0;
 }
-
+*/
