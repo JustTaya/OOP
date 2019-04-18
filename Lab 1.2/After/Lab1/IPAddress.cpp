2d97d2fa4b210031fed89f059de81d3331b53306
Taya <justataya@gmail.com>
2019-04-18 21:46:51 +0300

    Progress commit.

--- Lab 1.2/After/Lab1/IPAddress.cpp
+++ Lab 1.2/After/Lab1/IPAddress.cpp
@@ -4,3 +4,71 @@
 
 #include "IPAddress.h"
 
+
+IPv4::IPv4(std::vector<unsigned> ip) : ip(std::move(ip)) {
+    setBinary();
+}
+
+std::string IPv4::getBinary() const {
+    return binary;
+}
+
+void IPv4::setBinary() {
+    std::string bin;
+    for (auto i:ip) {
+        std::bitset<8> toBin(i);
+        bin += toBin.to_string();
+    }
+    binary = bin;
+}
+
+IPv6::IPv6(std::vector<unsigned> ip) : ip(std::move(ip)) {
+    setBinary();
+}
+
+std::string IPv6::getBinary() const {
+    return binary;
+}
+
+void IPv6::setBinary() {
+    std::string bin;
+    for (auto i:ip) {
+        std::bitset<16> toBin(i);
+        bin += toBin.to_string();
+    }
+    binary = bin;
+}
+
+IPv4::IPv4(std::vector<unsigned> ip) : ip(std::move(ip)) {
+    setBinary();
+}
+
+std::string IPv4::getBinary() const {
+    return binary;
+}
+
+void IPv4::setBinary() {
+    std::string bin;
+    for (auto i:ip) {
+        std::bitset<8> toBin(i);
+        bin += toBin.to_string();
+    }
+    binary = bin;
+}
+
+IPv6::IPv6(std::vector<unsigned> ip) : ip(std::move(ip)) {
+    setBinary();
+}
+
+std::string IPv6::getBinary() const {
+    return binary;
+}
+
+void IPv6::setBinary() {
+    std::string bin;
+    for (auto i:ip) {
+        std::bitset<16> toBin(i);
+        bin += toBin.to_string();
+    }
+    binary = bin;
+}
--- Lab 1.2/After/Lab1/IPAddress.h
+++ Lab 1.2/After/Lab1/IPAddress.h
@@ -5,54 +5,80 @@
 #ifndef LAB1_IPADDRESS_H
 #define LAB1_IPADDRESS_H
 
+#include "IPParser.h"
+
 #include <string>
 #include <vector>
+#include <memory>
+#include <bitset>
+
 
 //interface for IPs
 class IP {
 public:
     virtual ~IP() = default;
 
-    virtual std::string getBinary() = 0;
+    virtual std::string getBinary() const = 0;
 
 private:
-    virtual std::string setBinary() = 0;
+    virtual void setBinary() = 0;
 };
 
 class IPv4 : public IP {
 public:
-    ~IPv4();
+    IPv4() = delete;
 
-    std::string getBinary() override;
+    ~IPv4() = default;
+
+    std::string getBinary() const override;
+
+    friend class IPFactory;
 
 private:
     std::vector<unsigned> ip;
     std::string binary;
 
+    explicit IPv4(std::vector<unsigned> ip);
+
     friend class IPFactory;
 
-    IPv4(std::vector<unsigned> &ip);
-
-    std::string setBinary() override;
+    void setBinary() override;
 };
 
 class IPv6 : public IP {
 public:
-    ~IPv6();
+    IPv6() = delete;
 
-    std::string getBinary() override;
+    ~IPv6() = default;
 
-private:
-
-    std::vector<unsigned> ip;
-    std::string binary;
+    std::string getBinary() const override;
 
     friend class IPFactory;
 
-    IPv6(std::vector<unsigned> &ip);
+private:
+    std::vector<unsigned> ip;
+    std::string binary;
 
-    std::string setBinary() override;
+    explicit IPv6(std::vector<unsigned> ip);
+
+    void setBinary() override;
+};
+/*
+class IPFactoty {
+    static std::unique_ptr<IP> newIP(const std::string &ip);
 };
 
-
+std::unique_ptr<IP> IPFactoty::newIP(const std::string &ip) {
+    static IPParser parser = IPParser();
+    auto parsed = parser.parse(ip);
+    if (parsed.empty())
+        return nullptr;
+    switch (parsed.size()) {
+        case 4:
+            return std::make_unique<IP>(IPv4(parsed));
+        case 8:
+            return std::make_unique<IP>(IPv6(parsed));
+    }
+}
+*/
 #endif //LAB1_IPADDRESS_H
