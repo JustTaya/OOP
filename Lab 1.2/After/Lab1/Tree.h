fb3c90e15b29eaaee5152a8126682e95084b32a3
Unknown <justataya@gmail.com>
2019-05-26 15:48:48 +0300

    Progress commit

--- Lab 1.2/After/Lab1/Tree.h
+++ Lab 1.2/After/Lab1/Tree.h
@@ -89,9 +89,9 @@ AbstractNode<Key> *BinTree<Key, Cmp>::getRoot() const {
 template<typename Key, typename Cmp>
 class BSTree : public BinTree<Key, Cmp> {
 public:
-    BSTree() : BinTree<Key,Cmp>() {};
+    BSTree() : BinTree<Key, Cmp>() {};
 
-    explicit BSTree(Key key) : BinTree<Key,Cmp>(key) {};
+    explicit BSTree(Key key) : BinTree<Key, Cmp>(key) {};
 
     ~BSTree() override = default;
 
@@ -105,21 +105,126 @@ public:
 
     bool deleteNode(const Key &key) override;
 
+private:
+
+    BinaryNode<Key> *_deleteNode(BinaryNode<Key> *node, const Key &key);
+
+    BinaryNode<Key> *_findMin(BinaryNode<Key> *node);
+
+    BinaryNode<Key> *_deleteMin(BinaryNode<Key> *node);
+
+    bool _insertNode(BinaryNode<Key> *node);
+
 };
 
 template<typename Key, typename Cmp>
 AbstractNode<Key> *BSTree<Key, Cmp>::search(const Key &key) {
-    auto current = getRoot();
+    Cmp cmp;
+    auto current = this->getRoot();
     while (current != nullptr) {
-        if (current->getKey() > key) {
+        if (cmp(key, current->getKey())) {
             current = current->getLeft();
-        } else if (Cmp.cmp(current->getKey() < key)) {
+        } else if (cmp(current->getKey(), key)) {
             current = current->getRight();
         } else {
             return current;
         }
     }
     return nullptr;
+}
+
+template<typename Key, typename Cmp>
+bool BSTree<Key, Cmp>::deleteNode(const Key &key) {
+    this->root = _deleteNode(this->root, key);
+}
+
+template<typename Key, typename Cmp>
+BinaryNode<Key> *BSTree<Key, Cmp>::_deleteNode(BinaryNode<Key> *node, const Key &key) {
+    if (node == nullptr) {
+        return nullptr;
+    }
+    Cmp cmp;
+    if (cmp(key, node->getKey())) {
+        node->setLeft(_deleteNode(node->getLeft(), key));
+    } else if (cmp(node->getKey(), key)) {
+        node->setRight(_deleteNode(node->getRight(), key));
+    } else {
+        auto left = node->getLeft();
+        auto right = node->getRight();
+        delete node;
+        if (right == nullptr) {
+            return left;
+        }
+        auto min = _finMin(right);
+        min.right = _deleteMin(right);
+        min.left = left;
+        return min;
+    }
+}
+
+template<typename Key, typename Cmp>
+BinaryNode<Key> *BSTree<Key, Cmp>::_findMin(BinaryNode<Key> *node) {
+    auto current = node;
+    while (current->getLeft() != nullptr) {
+        current = current->getLeft();
+    }
+    return current;
+}
+
+template<typename Key, typename Cmp>
+BinaryNode<Key> *BSTree<Key, Cmp>::_deleteMin(BinaryNode<Key> *node) {
+    if (node->getLeft() == nullptr) {
+        return node->getRight();
+    }
+    node->setLeft(_deleteMin(node->getLeft()));
+    return node;
+}
+
+template<typename Key, typename Cmp>
+bool BSTree<Key, Cmp>::insert(const Key &key, std::string &path, const char delimiter, AbstractNode<Key> *node) {
+    return false;
+}
+
+template<typename Key, typename Cmp>
+bool BSTree<Key, Cmp>::insert(const Key &key, const std::vector<unsigned> &path, AbstractNode<Key> *node) {
+    return false;
+}
+
+template<typename Key, typename Cmp>
+bool BSTree<Key, Cmp>::insert(const Key &key, AbstractNode<Key> *node) {
+    auto newNode = new BinaryNode<Key>(key, nullptr);
+    _insertNode(node);
+}
+
+template<typename Key, typename Cmp>
+bool BSTree<Key, Cmp>::_insertNode(BinaryNode<Key> *node) {
+    Cmp cmp;
+    if (this->root == nullptr) {
+        this->root = node;
+    } else {
+        auto current = this->root;
+        while (current != nullptr) {
+            if (cmp(node->getKey(), current->getKey())) {
+                if (current->getLeft() == nullptr) {
+                    current->left = node;
+                    node->setParent(current);
+                    break;
+                } else {
+                    current = current->getLeft();
+                }
+            }
+            else{
+                if(current->getRight()==nullptr){
+                    current->setRight(node);
+                    node->setParent(current);
+                    break;
+                }
+                else{
+                    current=current->getRight();
+                }
+            }
+        }
+    }
 }
 
 
